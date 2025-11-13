ec1337afeb63bf7a2795f4c4a14a45b0c6b5282e
Vlad Flore <flore.vlad@gmail.com>
2025-11-13 12:37:15 +0100

    Publish blog update

--- filingcabinet/codebox/js.js
+++ filingcabinet/codebox/js.js
@@ -30,6 +30,8 @@ const GITHUB_USER = "vladflore";
 const GITHUB_REPO = "coding-challenges";
 const GITHUB_BRANCH = "refactor";
 
+const EXCLUDED_FILES = ["find.py"];
+
 let currentFile = null;
 let files = [];
 let filteredFiles = [];
@@ -82,7 +84,8 @@ async function fetchRepoFiles() {
     files = data.tree.filter(
       (f) =>
         f.type === "blob" &&
-        (f.path.endsWith(".py") || f.path.endsWith(".java"))
+        (f.path.endsWith(".py") || f.path.endsWith(".java")) &&
+        !EXCLUDED_FILES.includes(f.path.split("/").pop())
     );
 
     filteredFiles = [...files];
